a957ab168ad2cff1884f28e4930661c774e679ac
Wu Jane <jane72418@gmail.com>
2022-08-21 16:31:48 +0800

    add text editor

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,120 @@
-console.log('Hello!');
+console.log("Hello!");
 
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+  console.log("HesSchool Hello!");
+});
+
+CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
+  // https://ckeditor.com/docs/ckeditor5/latest/features/toolbar/toolbar.html#extended-toolbar-configuration-format
+  toolbar: {
+    items: [
+      "heading",
+      "bold",
+      "italic",
+      "bulletedList",
+      "numberedList",
+      "uploadImage",
+      "link",
+      "code",
+    ],
+    shouldNotGroupWhenFull: true,
+  },
+  // Changing the language of the interface requires loading the language file using the <script> tag.
+  // language: 'es',
+  list: {
+    properties: {
+      styles: true,
+      startIndex: true,
+      reversed: true,
+    },
+  },
+  // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html#configuration
+  heading: {
+    options: [
+      { model: "paragraph", title: "Paragraph", class: "ck-heading_paragraph" },
+      {
+        model: "heading1",
+        view: "h1",
+        title: "Heading 1",
+        class: "ck-heading_heading1",
+      },
+      {
+        model: "heading2",
+        view: "h2",
+        title: "Heading 2",
+        class: "ck-heading_heading2",
+      },
+      {
+        model: "heading3",
+        view: "h3",
+        title: "Heading 3",
+        class: "ck-heading_heading3",
+      },
+      {
+        model: "heading4",
+        view: "h4",
+        title: "Heading 4",
+        class: "ck-heading_heading4",
+      },
+      {
+        model: "heading5",
+        view: "h5",
+        title: "Heading 5",
+        class: "ck-heading_heading5",
+      },
+      {
+        model: "heading6",
+        view: "h6",
+        title: "Heading 6",
+        class: "ck-heading_heading6",
+      },
+    ],
+  },
+  // https://ckeditor.com/docs/ckeditor5/latest/features/editor-placeholder.html#using-the-editor-configuration
+  // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-family-feature
+  link: {
+    decorators: {
+      addTargetToExternalLinks: true,
+      defaultProtocol: "https://",
+      toggleDownloadable: {
+        mode: "manual",
+        label: "Downloadable",
+        attributes: {
+          download: "file",
+        },
+      },
+    },
+  },
+  placeholder: "Reply an answer…",
+  // https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html#configuration
+
+  // The "super-build" contains more premium features that require additional configuration, disable them below.
+  // Do not turn them on unless you read the documentation and know how to configure them and setup the editor.
+  removePlugins: [
+    // These two are commercial, but you can try them out without registering to a trial.
+    // 'ExportPdf',
+    // 'ExportWord',
+    "CKBox",
+    "CKFinder",
+    "EasyImage",
+    // This sample uses the Base64UploadAdapter to handle image uploads as it requires no configuration.
+    // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/base64-upload-adapter.html
+    // Storing images as Base64 is usually a very bad idea.
+    // Replace it on production website with other solutions:
+    // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/image-upload.html
+    // 'Base64UploadAdapter',
+    "RealTimeCollaborativeComments",
+    "RealTimeCollaborativeTrackChanges",
+    "RealTimeCollaborativeRevisionHistory",
+    "PresenceList",
+    "Comments",
+    "TrackChanges",
+    "TrackChangesData",
+    "RevisionHistory",
+    "Pagination",
+    "WProofreader",
+    // Careful, with the Mathtype plugin CKEditor will not load when loading this sample
+    // from a local file system (file://) - load this site via HTTP server if you enable MathType
+    "MathType",
+  ],
 });
